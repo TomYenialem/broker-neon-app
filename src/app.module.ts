0ab38d758f7ad03a800c242da0f682e486ff8912
yohanes1m <johnyemande@gmail.com>
2025-10-07 01:27:33 +0300

    feat: integrate all modules with file upload support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CarListingsModule } from './car-listings/car-listings.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { CommonModule } from './common/common.module';
+import { AuthModule } from './auth/auth.module';
+import { CarListingsModule } from './car-listings/car-listings.module';
 import { LandListingsModule } from './land-listings/land-listings.module';
+import { HouseListingsModule } from './house-listings/house-listings.module';
+import { MachineListingsModule } from './machine-listings/machine-listings.module';
+import { ListingsModule } from './listings/listings.module';
 
 @Module({
-  imports: [CarListingsModule,PrismaModule, LandListingsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CommonModule,
+    PrismaModule,
+    AuthModule,
+    ListingsModule,
+    CarListingsModule,
+    LandListingsModule,
+    HouseListingsModule,
+    MachineListingsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
