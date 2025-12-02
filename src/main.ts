9990d59a706b5ad3975482f950b0556ce4179dfa
TomYenialem <tom21g2008@gmail.com>
2025-12-02 14:10:32 +0300

    add frontedn url

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000'||'https://plancabrokers.vercel.app',
     credentials: true,
   });
 
